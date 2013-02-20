7d34fdff8d4329b72f5be3b086c64b3258ad99f7
Alex T <atroshko@cs.karelia.ru>
2013-02-20 15:30:50 +0400

    Icon, menus

--- gen/org/fruct/oss/visitkarelia/R.java
+++ gen/org/fruct/oss/visitkarelia/R.java
@@ -14,7 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int add_data=0x7f070004;
         public static final int container=0x7f070000;
+        public static final int menu_favourites=0x7f070003;
+        public static final int menu_map=0x7f070002;
         public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
@@ -38,15 +41,16 @@ public final class R {
         public static final int actionbar_map=0x7f040002;
         public static final int actionbar_nearby=0x7f040003;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f04000a;
+        public static final int hello_world=0x7f04000b;
+        public static final int menu_add_data=0x7f040007;
         public static final int menu_settings=0x7f040006;
-        public static final int title_activity_favour=0x7f04000d;
-        public static final int title_activity_map=0x7f04000b;
-        public static final int title_activity_nearby=0x7f04000c;
-        public static final int title_activity_travellog=0x7f04000e;
-        public static final int title_section1=0x7f040009;
-        public static final int title_section2=0x7f040008;
-        public static final int title_section3=0x7f040007;
+        public static final int title_activity_favour=0x7f04000e;
+        public static final int title_activity_map=0x7f04000c;
+        public static final int title_activity_nearby=0x7f04000d;
+        public static final int title_activity_travellog=0x7f04000f;
+        public static final int title_section1=0x7f04000a;
+        public static final int title_section2=0x7f040009;
+        public static final int title_section3=0x7f040008;
     }
     public static final class style {
         /** 
